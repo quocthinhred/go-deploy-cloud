0097db5b56d6ad2e30fc3bb73a046edfb491bcc7
thuocsi.thinhtq <thinh.tran@thuocsi.vn>
2022-11-16 17:33:40 +0700

    fix bug port

--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"example.com/micro/action"
 	"example.com/micro/client/login"
+	"fmt"
 	"gitlab.com/thuocsi.vn-sdk/go-sdk/sdk"
+	"log"
+	"net/http"
+	"os"
 )
 
 var app *sdk.App
@@ -19,6 +23,12 @@ func main() {
 	work = work.SetDelay(1)
 	work = work.SetRepeatPeriod(24 * 60 * 60)
 	work = work.SetTask(action.AutoLoginTask)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 
 	app.Launch()
 }
